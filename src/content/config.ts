dc5f1c4dd35c6dc8d924f43493cf9c00bddf7183
Gregor Obreza <gobreza@gmail.com>
2023-12-29 23:07:34 +0100

    added projects and fiished home page

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const projectsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
+      tags: z.array(z.string()),
+      featured:z.boolean()
+    })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  "projects": projectsCollection,
+};
---
+++ src/content/projects/project-3.md
@@ -0,0 +1,12 @@
+---
+title: My Third Project
+author: Astro Learner
+description: "I had some challenges, but asking in the community really helped!"
+image:
+    url: '/src/images/sample-project-image.png'
+    alt: "Thumbnail of Astro rays."
+pubDate: 2022-07-15
+tags: ["astro", "learning in public", "setbacks", "community"]
+featured: true
+---
+It wasn't always smooth sailing, but I'm enjoying building with Astro. And, the [Discord community](https://astro.build/chat) is really friendly and helpful!
