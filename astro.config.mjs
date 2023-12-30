e6321f7f21765994312e0ba948c9301730c13a71
Gregor Obreza <gobreza@gmail.com>
2023-12-30 23:45:04 +0100

    breadcrubs wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false,
-  })]
+    applyBaseStyles: false
+  }), sitemap()],
+  redirects: {
+    '/projects-tags': '/projects'
+  }
 });
