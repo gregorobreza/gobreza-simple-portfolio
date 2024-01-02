14f74039a4bccd8c4350cf78d44e4d5254d04751
Gregor Obreza <gobreza@gmail.com>
2024-01-02 12:19:28 +0100

    almost done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,29 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false
-  }), sitemap()],
+  markdown: {
+    // Applied to .md and .mdx files
+    // remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeAccessibleEmojis],
+  },
+  integrations: [
+    mdx({
+      rehypePlugins: [rehypeAccessibleEmojis],
+    }),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    
+  ],
   redirects: {
-    '/projects-tags': '/projects'
-  }
+    "/projects-tags": "/projects",
+  },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -2,21 +2,30 @@
 import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const projectsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string()),
-      featured:z.boolean()
-    })
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+    featured: z.boolean(),
+    githubLink: z.optional(z.string()),
+    projectLink: z.optional(z.string()),
+  }),
+});
+const aboutCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+  }),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-  "projects": projectsCollection,
+  projects: projectsCollection,
+  about: aboutCollection,
 };
