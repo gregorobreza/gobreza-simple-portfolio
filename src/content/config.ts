108f56860aae3567b31872ea2e59c7cd6932eebd
Gregor Obreza <gobreza@gmail.com>
2024-01-06 16:19:10 +0100

    simplified image passing

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,20 +3,22 @@ import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const projectsCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string(),
+      cover: image(),
+      // .refine((img) => img.width >= 1080, {
+      //   message: "Cover image must be at least 1080 pixels wide!",
+      // }),
+      coverAlt: z.string(),
+      tags: z.array(z.string()),
+      featured: z.boolean(),
+      githubLink: z.optional(z.string()),
+      projectLink: z.optional(z.string()),
     }),
-    tags: z.array(z.string()),
-    featured: z.boolean(),
-    githubLink: z.optional(z.string()),
-    projectLink: z.optional(z.string()),
-  }),
 });
 const aboutCollection = defineCollection({
   type: "content",
